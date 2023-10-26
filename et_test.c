3baec862ab4b9cf06aeb0653ce629406fca6b8fb
Nide17 <nidedrogba@gmail.com>
2023-10-26 20:10:26 +0000

    26 10 2023

--- et_test.c
+++ et_test.c
@@ -13,6 +13,7 @@
 #include <string.h> // strlen
 #include <ctype.h>  // isblank
 #include <math.h>   // fabs
+#include <stdbool.h>
 
 #include "expr_tree.h"
 
@@ -35,6 +36,12 @@
 int test_node_free()
 {
   ExprTree tree = NULL;
+
+  test_assert(ET_depth(tree) == 0);
+
+  tree = ET_value(0);
+  test_assert(ET_depth(tree) == 1);
+  ET_free(tree);
 
   tree = ET_value(23400000);
   test_assert(ET_depth(tree) == 1);
@@ -76,6 +83,10 @@ int test_depth()
 {
   ExprTree tree = NULL;
   int depth;
+
+  tree = ET_value(0);
+  test_assert(ET_depth(tree) == 1);
+  ET_free(tree);
 
   tree = ET_value(23400000);
   test_assert(ET_depth(tree) == 1);
@@ -174,6 +185,11 @@ int test_evaluate()
   ExprTree tree = NULL;
   int value = 0;
 
+  tree = ET_value(-1000);
+  value = ET_evaluate(tree);
+  ET_free(tree);
+  test_assert(value == -1000);
+
   tree = ET_node(OP_ADD, ET_value(1), ET_value(3));
   value = ET_evaluate(tree);
   ET_free(tree);
@@ -259,149 +275,106 @@ int test_evaluate()
  *
  * Returns: 1 if all tests pass, 0 otherwise
  */
-int test_tree2string()
+
+// test_tree2string_once
+bool test_tree2string_once(ExprTree tree, const char *expected_str)
 {
-  ExprTree tree = NULL;
   char buf[128];
   double result;
 
-  tree = ET_node(OP_ADD, ET_value(1), ET_value(3));
   ET_tree2string(tree, buf, sizeof(buf));
   result = ET_evaluate(tree);
-  ET_free(tree);
   printf("%s ==> %g\n", buf, result);
-  test_assert(strcmp(buf, "1 + 3") == 0);
+  return strcmp(buf, expected_str) == 0;
+}
+
+int test_tree2string()
+{
+  ExprTree tree = NULL;
+
+  tree = ET_node(OP_ADD, ET_value(1), ET_value(3));
+  test_assert(test_tree2string_once(tree, "(1 + 3)"));
+  ET_free(tree);
 
   tree = ET_node(OP_MUL, ET_value(5), ET_node(OP_SUB, ET_value(10), ET_value(3)));
-  result = ET_evaluate(tree);
-  ET_tree2string(tree, buf, sizeof(buf));
+  test_assert(test_tree2string_once(tree, "(5 * (10 - 3))"));
   ET_free(tree);
-  printf("%s ==> %g\n", buf, result);
-  test_assert(strcmp(buf, "(5 * (10 - 3))") == 0);
 
   tree = ET_node(OP_POWER, ET_value(2), ET_value(3));
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
+  test_assert(test_tree2string_once(tree, "(2 ^ 3)"));
   ET_free(tree);
-  test_assert(strcmp(buf, "2 ^ 3") == 0);
 
   // -0.5 (as a value)
   double value1 = -0.5;
   tree = ET_value(value1);
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
+  test_assert(test_tree2string_once(tree, "-0.5"));
   ET_free(tree);
-  test_assert(strcmp(buf, "-0.5") == 0);
 
   // (6.5 * (4 + 3))
   tree = ET_node(OP_ADD, ET_value(4), ET_value(3));
   tree = ET_node(OP_MUL, ET_value(6.5), tree);
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
+  test_assert(test_tree2string_once(tree, "(6.5 * (4 + 3))"));
   ET_free(tree);
-  test_assert(strcmp(buf, "(6.5 * (4 + 3))") == 0);
 
   // -0.125 (as a value)
-  const double value = -0.125;
+  double value = -0.125;
   tree = ET_value(value);
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
-  ET_free(tree);
-  test_assert(strcmp(buf, "-0.125") == 0);
-
+  test_assert(test_tree2string_once(tree, "-0.125"));
+  
   // -(-0.125) (using value and unary negation) -> (--0.125) ==> 0.125
-  tree = ET_node(UNARY_NEGATE, ET_value(value), NULL);
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
-  test_assert(strcmp(buf, "(--0.125)") == 0);
+  tree = ET_node(UNARY_NEGATE, tree, NULL);
+  test_assert(test_tree2string_once(tree, "(--0.125)"));
 
   // double unary negation: (- (- (-0.125))) -> (-(--0.125)) ==> -0.125
   tree = ET_node(UNARY_NEGATE, tree, NULL);
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
+  test_assert(test_tree2string_once(tree, "(-(--0.125))"));
   ET_free(tree);
-  test_assert(strcmp(buf, "(-(--0.125))") == 0);
+
+  // ((20 - 10) - (30 - 25))
+  tree = ET_node(OP_SUB, ET_value(20), ET_value(10));
+  tree = ET_node(OP_SUB, tree, ET_node(OP_SUB, ET_value(30), ET_value(25)));
+  test_assert(test_tree2string_once(tree, "((20 - 10) - (30 - 25))"));
+  ET_free(tree);
+
+  // ((1 + 2) * (3 - 4))
+  tree = ET_node(OP_SUB, ET_value(3), ET_value(4));
+  tree = ET_node(OP_MUL, ET_node(OP_ADD, ET_value(1), ET_value(2)), tree);
+  test_assert(test_tree2string_once(tree, "((1 + 2) * (3 - 4))"));
+  ET_free(tree);
 
   // (( 2 ^ 3) / (1.3 + 2.7))
   tree = ET_node(OP_ADD, ET_value(1.3), ET_value(2.7));
   tree = ET_node(OP_DIV, ET_node(OP_POWER, ET_value(2), ET_value(3)), tree);
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
+  test_assert(test_tree2string_once(tree, "((2 ^ 3) / (1.3 + 2.7))"));
   ET_free(tree);
-  test_assert(strcmp(buf, "((2 ^ 3) / (1.3 + 2.7))") == 0);
-
-  // (20 - 10) - (30 - 25)
-  tree = ET_node(OP_SUB, ET_value(20), ET_value(10));
-  tree = ET_node(OP_SUB, tree, ET_node(OP_SUB, ET_value(30), ET_value(25)));
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
-  ET_free(tree);
-  test_assert(strcmp(buf, "((20 - 10) - (30 - 25))") == 0);
-
-  // ((2 ^ (1.5 × 2)) ÷ (−1.7 + (6.0 − 0.3)))
-  tree = ET_node(OP_DIV, ET_node(OP_POWER, ET_value(2), ET_node(OP_MUL, ET_value(1.5), ET_value(2))), ET_node(OP_ADD, ET_value(-1.7), ET_node(OP_SUB, ET_value(6.0), ET_value(0.3))));
-  result = ET_evaluate(tree);
-  ET_tree2string(tree, buf, sizeof(buf));
-  printf("%s ==> %g\n", buf, result);
-  ET_free(tree);
-  // test_assert(strcmp(buf, "((2 ^ (1.50 * 2) / (-1.70 + (6 - 0.3)))") == 0);
 
   // other complex and corner cases to test:
-  // (1 + 2) * (3 - 4)
-  tree = ET_node(OP_SUB, ET_value(3), ET_value(4));
-  tree = ET_node(OP_MUL, ET_node(OP_ADD, ET_value(1), ET_value(2)), tree);
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
+  tree = ET_node(OP_ADD, ET_value(-2), ET_value(3));
+  tree = ET_node(OP_MUL, tree, ET_node(OP_DIV, ET_node(OP_SUB, ET_value(-4), ET_value(1)), ET_value(2)));
+  test_assert(test_tree2string_once(tree, "((-2 + 3) * ((-4 - 1) / 2))"));
   ET_free(tree);
-  test_assert(strcmp(buf, "((1 + 2) * (3 - 4))") == 0);
 
-  // (5 * (3 + 7)) / ((2 - 1) * 4)
-  tree = ET_node(OP_SUB, ET_value(2), ET_value(1));
-  tree = ET_node(OP_MUL, tree, ET_value(4));
-  tree = ET_node(OP_DIV, ET_node(OP_MUL, ET_value(5), ET_node(OP_ADD, ET_value(3), ET_value(7))), tree);
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
-  ET_free(tree);
-  // test_assert(strcmp(buf, "((5 * (3 + 7)) / ((2 - 1) * 4))") == 0);
-
-  // ((2 + 3) ^ 2) / ((4 - 1) * 2)
   tree = ET_node(OP_SUB, ET_value(4), ET_value(1));
   tree = ET_node(OP_MUL, tree, ET_value(2));
   tree = ET_node(OP_DIV, ET_node(OP_POWER, ET_node(OP_ADD, ET_value(2), ET_value(3)), ET_value(2)), tree);
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
+  test_assert(test_tree2string_once(tree, "(((2 + 3) ^ 2) / ((4 - 1) * 2))"));
   ET_free(tree);
-  // test_assert(strcmp(buf, "(((2 + 3) ^ 2) / ((4 - 1) * 2))") == 0);
 
-  // (-2 + 3) * ((-4 - 1) / 2)
-  tree = ET_node(OP_DIV, ET_node(OP_SUB, ET_value(-4), ET_value(1)), ET_value(2));
-  tree = ET_node(OP_MUL, ET_node(OP_ADD, ET_value(-2), ET_value(3)), tree);
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
+  tree = ET_node(OP_SUB, ET_value(2), ET_value(1));
+  tree = ET_node(OP_MUL, tree, ET_value(4));
+  tree = ET_node(OP_DIV, ET_node(OP_MUL, ET_value(5), ET_node(OP_ADD, ET_value(3), ET_value(7))), tree);
+  test_assert(test_tree2string_once(tree, "((5 * (3 + 7)) / ((2 - 1) * 4))"));
   ET_free(tree);
-  // test_assert(strcmp(buf, "((-2 + 3) * ((-4 - 1) / 2))") == 0);
 
-  // (2 + (-3 ^ 2)) * ((-4 + 1) / 2)
+  tree = ET_node(OP_DIV, ET_node(OP_POWER, ET_value(2), ET_node(OP_MUL, ET_value(1.5), ET_value(2))), ET_node(OP_ADD, ET_value(-1.7), ET_node(OP_SUB, ET_value(6), ET_value(0.3))));
+  test_assert(test_tree2string_once(tree, "((2 ^ (1.5 * 2)) / (-1.7 + (6 - 0.3)))"));
+  ET_free(tree);
+
   tree = ET_node(OP_ADD, ET_value(-4), ET_value(1));
   tree = ET_node(OP_DIV, tree, ET_value(2));
   tree = ET_node(OP_MUL, ET_node(OP_ADD, ET_value(2), ET_node(OP_POWER, ET_value(-3), ET_value(2))), tree);
-  ET_tree2string(tree, buf, sizeof(buf));
-  result = ET_evaluate(tree);
-  printf("%s ==> %g\n", buf, result);
+  test_assert(test_tree2string_once(tree, "((2 + (-3 ^ 2)) * ((-4 + 1) / 2))"));
   ET_free(tree);
-  // test_assert(strcmp(buf, "((2 + (-3 ^ 2)) * ((-4 + 1) / 2))") == 0);
 
   return 1;
 }
--- expr_tree.c
+++ expr_tree.c
@@ -8,7 +8,6 @@
  * Author: Howdy Pierce <howdy@sleepymoose.net>
  * Contributor: Niyomwungeri Parmenide Ishimwe <parmenin@andrew.cmu.edu>
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -85,14 +84,14 @@ ExprTree ET_node(ExprNodeType op, ExprTree left, ExprTree right)
 void ET_free(ExprTree tree)
 {
   if (tree == NULL)
-  {
     return;
-  }
+
   if (tree->type != VALUE)
   {
     ET_free(tree->n.child[LEFT]);
     ET_free(tree->n.child[RIGHT]);
   }
+
   free(tree);
 }
 
@@ -100,26 +99,22 @@ void ET_free(ExprTree tree)
 int ET_count(ExprTree tree)
 {
   if (tree == NULL)
-  {
     return 0;
-  }
+
   if (tree->type == VALUE)
-  {
     return 1;
-  }
+
   return 1 + ET_count(tree->n.child[LEFT]) + ET_count(tree->n.child[RIGHT]);
 }
 // Documented in .h file
 int ET_depth(ExprTree tree)
 {
   if (tree == NULL)
-  {
     return 0;
-  }
+
   if (tree->type == VALUE)
-  {
     return 1;
-  }
+
   int left = ET_depth(tree->n.child[LEFT]);
   int right = ET_depth(tree->n.child[RIGHT]);
   return 1 + (left > right ? left : right);
@@ -129,15 +124,14 @@ int ET_depth(ExprTree tree)
 double ET_evaluate(ExprTree tree)
 {
   if (tree == NULL)
-  {
     return 0;
-  }
+
   if (tree->type == VALUE)
-  {
     return tree->n.value;
-  }
+
   double left = ET_evaluate(tree->n.child[LEFT]);
   double right = ET_evaluate(tree->n.child[RIGHT]);
+
   switch (tree->type)
   {
   case UNARY_NEGATE:
@@ -178,25 +172,26 @@ double ET_evaluate(ExprTree tree)
  *   If the string was truncated due to insufficient buffer space, the last
  *   character will be '$'.
  */
-// size_t ET_tree2string(ExprTree tree, char *buf, size_t buf_sz);
-// Write a number to the buffer, handling truncation
-// Returns the number of characters written
-static size_t writeNumberToBuffer(double number, char *buf, size_t buf_sz)
+static size_t writeValueToBuffer(double number, char *buf, size_t buf_sz)
 {
   size_t length;
 
-  // Check if the number is an integer (has no fractional part)
+  if (snprintf(buf, buf_sz, "%f", number) >= buf_sz)
+  {
+    buf[buf_sz - 2] = '$';
+    buf[buf_sz - 1] = '\0';
+    return buf_sz - 1;
+  }
+
   if (fmod(number, 1.0) == 0.0)
   {
-    length = snprintf(buf, buf_sz, "%.0f", number); // Format as an integer
+    length = snprintf(buf, buf_sz, "%.0f", number);
   }
   else
   {
-    // Handle non-integer numbers
     char temp[buf_sz];
-    snprintf(temp, buf_sz, "%f", number); // Format as a decimal with one decimal place
+    snprintf(temp, buf_sz, "%f", number);
 
-    // Truncate the string at the first non-zero character from the end
     char *truncatePos = temp + strlen(temp) - 1;
     while (*truncatePos == '0')
     {
@@ -208,159 +203,79 @@ static size_t writeNumberToBuffer(double number, char *buf, size_t buf_sz)
     for (char *ptr = temp; *ptr; ptr++)
     {
       if (*ptr == '.')
+      {
         count = strlen(temp) - (ptr - temp) - 1;
+      }
     }
 
     if (count < 2)
-      length = snprintf(buf, buf_sz, "%.1f", number); // Format as a decimal with one decimal place
+    {
+      length = snprintf(buf, buf_sz, "%.1f", number);
+    }
     else
+    {
       length = snprintf(buf, buf_sz, "%s", temp);
-  }
-
-  return length;
-}
-
-// Write an expression to the buffer, handling truncation
-// Returns the number of characters written
-static size_t writeExprToBuffer(ExprTree tree, char *buf, size_t buf_sz)
-{
-  size_t length = 0;
-
-  if (tree->n.child[LEFT] == NULL)
-  {
-    // Leaf node: write the operator character
-    length = snprintf(buf, buf_sz, "%c", ExprNodeType_to_char(tree->type));
-  }
-  else
-  {
-    char leftBuffer[buf_sz / 2];
-    size_t leftLength = ET_tree2string(tree->n.child[LEFT], leftBuffer, buf_sz / 2);
-
-    if (tree->type == UNARY_NEGATE)
-    {
-      if (tree->n.child[LEFT]->type == UNARY_NEGATE)
-      {
-        // Omit extra parentheses for consecutive unary negations
-        length = snprintf(buf, buf_sz, "--%s", leftBuffer);
-      }
-      else
-      {
-        // Add parentheses for unary negation
-        length = snprintf(buf, buf_sz, "(-%s)", leftBuffer);
-      }
-    }
-
-    if (tree->n.child[RIGHT] == NULL)
-    {
-      // Unary operator or leaf node with a left child
-      length = snprintf(buf, buf_sz, "(%c%s)", ExprNodeType_to_char(tree->type), leftBuffer);
-    }
-    else
-    {
-      char rightBuffer[buf_sz / 2];
-      size_t rightLength = ET_tree2string(tree->n.child[RIGHT], rightBuffer, buf_sz / 2);
-
-      // Check if parentheses are needed around the left expression
-      if (tree->n.child[LEFT]->type != VALUE && tree->n.child[LEFT]->type != UNARY_NEGATE)
-      {
-        // Add parentheses
-        char tempBuffer[buf_sz / 2];
-        snprintf(tempBuffer, buf_sz / 2, "(%s)", leftBuffer);
-        strcpy(leftBuffer, tempBuffer);
-        leftLength += 2;
-      }
-
-      // Check if parentheses are needed around the right expression
-      if (tree->n.child[RIGHT]->type != VALUE)
-      {
-        if (tree->type != UNARY_NEGATE && (tree->type == OP_ADD || tree->type == OP_SUB || (tree->type == OP_MUL || tree->type == OP_DIV)))
-        {
-          // Add parentheses
-          char tempBuffer[buf_sz / 2];
-          snprintf(tempBuffer, buf_sz / 2, "(%s)", rightBuffer);
-          strcpy(rightBuffer, tempBuffer);
-          rightLength += 2;
-        }
-
-        // Handle OP_POWER
-        if (tree->type == OP_POWER)
-        {
-          // Add parentheses for the power operator
-          char tempBuffer[buf_sz / 2];
-          snprintf(tempBuffer, buf_sz / 2, "(%s)", rightBuffer);
-          strcpy(rightBuffer, tempBuffer);
-          rightLength += 2;
-        }
-      }
-
-      // Check if there's enough space in the buffer
-      if (length + leftLength + rightLength + 3 >= buf_sz)
-      {
-        // Handle truncation
-        length = snprintf(buf, buf_sz, "%.*s$", (int)(buf_sz - 2), leftBuffer);
-      }
-      else
-      {
-        if (tree->n.child[LEFT]->type != VALUE || tree->n.child[RIGHT]->type != VALUE)
-        {
-          // Add parentheses around the entire expression
-          length = snprintf(buf, buf_sz, "(%s %c %s)", leftBuffer, ExprNodeType_to_char(tree->type), rightBuffer);
-        }
-        else
-        {
-          length = snprintf(buf, buf_sz, "%s %c %s", leftBuffer, ExprNodeType_to_char(tree->type), rightBuffer);
-        }
-      }
     }
   }
 
   return length;
 }
 
-// Convert an ExprTree into a printable ASCII string stored in buf
-// Returns the number of characters written to buf, not counting the '\0' terminator.
-// If the string was truncated due to insufficient buffer space, the last character will be '$'.
 size_t ET_tree2string(ExprTree tree, char *buf, size_t buf_sz)
 {
-  // Check if the tree is NULL
   if (tree == NULL)
   {
-    // You can return an error indicator or a default string
     strncpy(buf, "Error: NULL tree", buf_sz - 1);
     buf[buf_sz - 1] = '\0';
     return strlen(buf);
   }
 
-  // Check if the buffer size is insufficient
-  if (buf_sz < 2)
-  {
-    buf[0] = '\0';
-    return 0;
-  }
-
-  // Initialize the length variable to keep track of characters written
   size_t length = 0;
 
   if (tree->type == VALUE)
   {
-    length = writeNumberToBuffer(tree->n.value, buf, buf_sz);
+    length = writeValueToBuffer(tree->n.value, buf, buf_sz);
   }
   else
   {
-    length = writeExprToBuffer(tree, buf, buf_sz);
 
-    // Check if the length exceeds the buffer size and append '$' if needed
-    if (length >= buf_sz)
+    char leftBuffer[buf_sz / 2];
+    size_t leftLength = ET_tree2string(tree->n.child[LEFT], leftBuffer, buf_sz / 2);
+    if (tree->type == UNARY_NEGATE)
     {
-      buf[buf_sz - 1] = '$';
-      // Adjust the length to the buffer size, excluding the truncation indicator
-      length = buf_sz - 1;
+      length = snprintf(buf, buf_sz, "(-%s)", leftBuffer);
+    }
+
+    else
+    {
+      char rightBuffer[buf_sz / 2];
+      size_t rightLength = ET_tree2string(tree->n.child[RIGHT], rightBuffer, buf_sz / 2);
+
+      if (tree->n.child[LEFT] != NULL && tree->n.child[LEFT]->type != VALUE)
+      {
+        char tempBuffer[buf_sz / 2];
+        snprintf(tempBuffer, buf_sz / 2, "%s", leftBuffer);
+        printf("leftBuffer: %s\n", tempBuffer);
+        strcpy(leftBuffer, tempBuffer);
+        leftLength += 2;
+      }
+
+      if (tree->n.child[RIGHT] != NULL && tree->n.child[RIGHT]->type != VALUE)
+      {
+        char tempBuffer[buf_sz / 2];
+        snprintf(tempBuffer, buf_sz / 2, "%s", rightBuffer);
+        printf("rightBuffer: %s\n", tempBuffer);
+        strcpy(rightBuffer, tempBuffer);
+        rightLength += 2;
+      }
+
+      length = snprintf(buf, buf_sz, "(%s %c %s)", leftBuffer, ExprNodeType_to_char(tree->type), rightBuffer);
     }
   }
 
-  // printf("length: %zu buf_sz: %zu\n", length, buf_sz);
   return length;
 }
 
-// EACH OPERATION IS WITHIN PARENTHESES -  HANDLE THAT
+// EACH OPERATION IS WITHIN PARENTHESES - HANDLE THAT
 // TEST EVALUATE VALUES
+// TRUNCATION
